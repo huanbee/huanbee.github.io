21ff15e0c4cce6ef2a9b209d8985a0d20ec1e083
Nguyen Huan <huanbee@users.noreply.github.com>
2020-10-01 15:45:53 +0700

    Update Poup.js

--- T3H/gt-game/src/view/layout/components/Poup.js
+++ T3H/gt-game/src/view/layout/components/Poup.js
@@ -4,42 +4,39 @@ import { Row, Col } from "react-bootstrap";
 
 class Popup extends React.Component {
   render() {
-    console.log("Props Item: ", this.props.item)
+  const {item}= this.props;
+    console.log("Props Item: ", this.props.item);
     return (
-      
       <div className="popup">
-        
         <div className="popup_inner">
-        <button onClick={this.props.closePopup}>×</button>
-        <div  className="popup_table">
-          <Row>
-            <Col sm={6} xs={12}>
-            <img src={this.props.item.src} className="zoom-img"/>
-            <div className="clearfix"></div>
-            </Col>
-            <Col sm={6} xs={12}>
-            <h4>{this.props.texth4}</h4>
-            <p >{this.props.textp1}</p>   
-          <div className="span span1">
-            <p className="left">NAME</p>
-            <p className="right">: {this.props.item.textName}</p>
-            <div className="clearfix"></div>
+          <button onClick={this.props.closePopup}>×</button>
+          <div className="popup_table">
+            <Row>
+              <Col sm={6} xs={12}>
+                <img src={this.props.item.src} className="zoom-img" />
+                <div className="clearfix"></div>
+              </Col>
+              <Col sm={6} xs={12}>
+                <h4>{this.props.texth4}</h4>
+                <p>{this.props.textp1}</p>
+                <div className="span span1">
+                  <p className="left">NAME</p>
+                  <p className="right">: {this.props.item.textName}</p>
+                  <div className="clearfix"></div>
+                </div>
+                <div className="span span2">
+                  <p className="left">DEVELOPER</p>
+                  <p className="right">: {this.props.textDeveloper}</p>
+                  <div className="clearfix"></div>
+                </div>
+                <div className="span span3">
+                  <p className="left">REQUIRES</p>
+                  <p className="right">: {this.props.textRequires}</p>
+                  <div className="clearfix"></div>
+                </div>
+              </Col>
+            </Row>
           </div>
-          <div className="span span2">
-            <p className="left">DEVELOPER</p>
-            <p className="right">: {this.props.textDeveloper}</p>
-            <div className="clearfix"></div>
-          </div>
-          <div className="span span3">
-            <p className="left">REQUIRES</p>
-            <p className="right">: {this.props.textRequires}</p>
-            <div className="clearfix"></div>
-          </div>
-            </Col>
-          </Row>
-          </div>
-
-         
         </div>
       </div>
     );
